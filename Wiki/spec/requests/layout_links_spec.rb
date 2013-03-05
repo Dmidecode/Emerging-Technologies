24f5c133de801ec2916126dc2e60042648f058fe
rails new railsinstaller_demo <benoit.mira@gmail.com>
2013-03-05 18:36:49 +0000

    Wiki Assigment

---
+++ Wiki/spec/requests/layout_links_spec.rb
@@ -0,0 +1,19 @@
+require 'spec_helper'
+
+describe "LayoutLinks" do
+	it "Home at '/'" do
+    get '/'
+    response.should have_selector('title', :content => "Home")
+  end
+
+  it "Description at '/description'" do
+    get '/description'
+    response.should have_selector('title', :content => "Description")
+  end
+
+  it "Stream at '/stream'" do
+    get '/stream'
+    response.should have_selector('title', :content => "Stream")
+  end
+
+end
---
+++ assigment1/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Assigment1::Application.config.secret_token = '35f3cdfb8b99a31d6c32f03b4e9cef94c364e8034249711a988f96c387ffcd71e5fda79543ec240909f2dc35b91b3979048b39eed68a56da6269470abe24e28e'
