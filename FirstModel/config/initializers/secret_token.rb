0fe515596a0eb49eae198d8eade7245a9e4a719b
rails new railsinstaller_demo <benoit.mira@gmail.com>
2013-03-11 16:38:18 +0000

    Erase Second branch

--- FirstModel/config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-FirstModel::Application.config.secret_token = '900408d39711d5d0d8e67d3c3d54432b52c97201155b9976b94867a23fef9f56b5c9c357189f803d5fd21ff4f0a57c6eb9fbce2343dcb9ff393b734d21c2ea0f'
--- demo_app/config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-DemoApp::Application.config.secret_token = 'ea1c79ac3ebb6bbdc29c10f28cf9e7277baef2e75f2a26020d975c10ce5d6008deca68ca87a43b88e67a3b0198fdfec5bae93ed32876bdd3f1fcdd63386976ec'
